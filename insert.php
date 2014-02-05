d42e04227170aa3ae2ba56b1647544c1999422d9
tomay <tom.allnutt@gmail.com>
2014-02-05 10:56:07 -0800

    updates per advice of jen/greg * now uses pg_query_params * echo's json instead of html

--- insert.php
+++ insert.php
@@ -1,32 +1,33 @@
 <?php
-$db = pg_connect("host=localhost port=5432 dbname=training user=postgres");
+$db_connection = pg_connect("host=localhost port=5432 dbname=training user=postgres");
 
-$lat = $_POST[latitude];
-$long = $_POST[longitude];
-$wkt = sprintf("SRID=4326;POINT(%s %s)",$long, $lat);
-$query = sprintf("INSERT INTO coords (sitename, description, site_id, visit, created_at, the_geom, latitude, longitude) 
-  VALUES ('$_POST[sitename]','$_POST[description]','$_POST[site_id]','$_POST[visit]','$_POST[created_at]',GeomFromEWKT('%s'),'$lat','$long')",$wkt);
+$lat = $_POST['latitude'];
+$long = $_POST['longitude'];
 
-$insert = pg_query($query);
+// %f will format 34 to 34.000000 and arbitrary strings to 0.00000
+$wkt = sprintf("GeomFromEWKT('SRID=4326;POINT(%f %f)')",$long, $lat);
+
+$geom = pg_escape_string($wkt);
+
+$query = 'INSERT INTO coords (sitename, description, site_id, visit, created_at, the_geom, latitude, longitude)
+  VALUES ($1, $2, $3, $4, $5, $6, $7, $8)';
+
+$params = array($_POST['sitename'],$_POST['description'],$_POST['site_id'],
+  $_POST['visit'],$_POST['created_at'],$wkt,'12.3232','143.3423');
+
+$insert = pg_query_params($db_connection, $query, $params);
 
 $query = 'select * from coords';
 $result = pg_query($query);
 
-   while ($row = pg_fetch_array($result)){
-    echo 'Primary key: ' .$row['id_key'];
-    echo '<br /> Sitename: ' .$row['sitename'];
-    echo '<br /> Description: '.$row['description'];
-    echo '<br /> Site Id: '.$row['site_id'];
-    echo '<br /> Created at: '.$row['created_at'];
-    echo '<br /> Visit: '.$row['visit'];
-    echo '<br /> Visit: '.$row['the_geom'];
-    echo '<br /> Latitude: '.$row['latitude'];
-    echo '<br /> Latitude: '.$row['longitude'];
-    echo '<br />';
-    echo '<br />';
-  }
+$rows = array();
+while($r = pg_fetch_assoc($result)) {
+  $rows[] = $r;
+}
+echo json_encode($rows);
 
-echo '</body></html>';
-echo "<a href='index.html'><< Back</a>"
+echo '</br>';
+echo '</br>';
+echo "<a href='index.html'><< Back</a>";
 
 ?>
